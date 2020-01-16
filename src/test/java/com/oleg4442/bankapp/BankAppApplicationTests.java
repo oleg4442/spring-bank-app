09c4bcfba297777e0826cf983d24ce532a6d4745
Oleg <o.s.naydin@gmail.com>
2020-01-16 23:44:44 +0100

    Spring intlzr bank-app project

---
+++ src/test/java/com/oleg4442/bankapp/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleg4442.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
